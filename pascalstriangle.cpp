3a4063f82e67cf2f0e21d102d1453cb11fb6466f
Gyanendra saurav <130287827+tezarfase03@users.noreply.github.com>
2023-08-28 17:10:16 +0530

    Add files via upload

---
+++ pascalstriangle.cpp
@@ -0,0 +1,16 @@
+//    for discription refer to leet code actually its a easy what is told in the question
+
+vector<vector<int>> generate(int numRows)
+{
+    vector<vector<int>> v;
+    for (int i = 0; i < numRows; i++)
+    {
+        vector<int> temp(i + 1, 1);
+        for (int j = 1; j < i; j++)
+        {
+            temp[j] = v[i - 1][j - 1] + v[i - 1][j];
+        }
+        v.push_back(temp);
+    }
+    return v;
+}
